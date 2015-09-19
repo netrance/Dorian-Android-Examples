d58e9e3b3d23f30547ea1128c9bd9e416c9497ea
Dohyoung Lee <netrance@naver.com>
2015-09-20 01:22:16 +0900

    New Project - DrawlerLayout Example
    This projects introduces an example of DrawlerLayout view. The view has
    two drawer views.  One is pulled out from the left edge of the window.
    And the other is done  from the right edge.

---
+++ ui_examples/android_studio/DrawerLayoutExample/app/src/main/java/me/blog/netrance/android/drawerlayout_example/MainActivity.java
@@ -0,0 +1,22 @@
+package me.blog.netrance.android.drawerlayout_example;
+
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.LinearLayout;
+
+/**
+ * This activity
+ */
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.layout_of_activity_main);
+    }
+
+}
