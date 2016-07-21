413800e2c8c68170eb599b97aa3b165496dc132a
Dorian Lee <dorian.lee@orangedigit.com>
2016-07-21 22:21:41 +0900

    New Example - Hiding Bars
    This example shows how to hide the status bar and the navigation bar.

---
+++ ui_examples/android_studio/HidingBars/app/src/main/java/cf/domone/android/hiding_bars/MainActivity.java
@@ -0,0 +1,22 @@
+package cf.domone.android.hiding_bars;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Hides the status bar and the navigation bar.
+        View decorView = getWindow().getDecorView();
+        decorView.setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_FULLSCREEN
+                | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+        );
+    }
+}
