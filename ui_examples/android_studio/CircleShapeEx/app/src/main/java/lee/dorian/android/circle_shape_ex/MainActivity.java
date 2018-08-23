1ff836c951a67ec26960aa3df42aa4533f0138a4
dorian <netrance@naver.com>
2018-08-23 18:18:51 +0900

    New Example - Circle Shape
    This example shows how to implement a circle shape on a view.

---
+++ ui_examples/android_studio/CircleShapeEx/app/src/main/java/lee/dorian/android/circle_shape_ex/MainActivity.java
@@ -0,0 +1,13 @@
+package lee.dorian.android.circle_shape_ex;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
